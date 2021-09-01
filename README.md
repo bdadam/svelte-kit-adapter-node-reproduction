b018c41a83ab6e79259bec8c1d65f307b678a979
Adam <me@bdadam.com>
2021-09-01 15:32:12 +0200

    Issue reproduction

---
+++ README.md
@@ -0,0 +1,11 @@
+# Reproduction
+
+```sh
+npm i
+
+npm run build
+```
+
+## Issue
+
+There is only an `index.js` file in the `dist` directory, although the adapter-node readme says a middleware should also be created.
---
+++ svelte.config.js
@@ -0,0 +1,20 @@
+import node from "@sveltejs/adapter-node";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+    adapter: node({
+      out: "dist",
+      esbuild(defaultOptions) {
+        return {
+          ...defaultOptions,
+          target: "node16",
+        };
+      },
+    }),
+  },
+};
+
+export default config;
